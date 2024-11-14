51b3a699feccbd90b843e6655e2ae7aecf5f8368
jggarzon <javiergarciagarzon@gmail.com>
2024-11-14 12:50:38 +0100

    Initial commit

---
+++ src/test/java/com/example/starshipManager/StarshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starshipManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
